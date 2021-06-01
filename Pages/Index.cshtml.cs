90fcd7e2e1ae520fc95bd3172364c5294d90fec3
Tyler Long <tyler4long@gmail.com>
2021-06-01 12:12:44 -0700

    First working version

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
+using RingCentral;
 
 namespace MyFirstAzureWebApp.Pages
 {
@@ -17,15 +18,30 @@ namespace MyFirstAzureWebApp.Pages
             _logger = logger;
         }
 
-        public void OnGet()
+        public async void OnGet()
         {
             var clientId = this.HttpContext.Request.Query["clientId"];
             var clientSecret = this.HttpContext.Request.Query["clientSecret"];
             var username = this.HttpContext.Request.Query["username"];
             var extension = this.HttpContext.Request.Query["extension"];
             var password = this.HttpContext.Request.Query["password"];
+            var from = this.HttpContext.Request.Query["from"];
             var to = this.HttpContext.Request.Query["to"];
-            Console.WriteLine(clientId + "" + clientSecret + "" + username + "" + extension + "" + password + "" + to);
+            Console.WriteLine(clientId + "" + clientSecret + "" + username + "" + extension + "" + password + "" + from + "" + to);
+
+            var rc = new RestClient(clientId, clientSecret, true);
+            try{
+            await rc.Authorize(username, extension, password);
+            Console.WriteLine(rc.token.access_token);
+
+            await rc.Restapi().Account().Extension().Sms().Post(new CreateSMSMessage{
+                text="Hello world from RingCentral hosted by Azure",
+                from=new MessageStoreCallerInfoRequest{phoneNumber=from},
+                to=new MessageStoreCallerInfoRequest[]{new MessageStoreCallerInfoRequest{phoneNumber=to}},
+            });
+            }catch(RestException re) {
+                Console.WriteLine(Utils.FormatHttpMessage(re.httpResponseMessage, re.httpRequestMessage));
+            }
         }
     }
 }
---
+++ README.md
@@ -0,0 +1,13 @@
+# RingCentral Azure Web App Demo
+
+Specify the following credentials in query parameters:
+
+- clientId
+- clientSecret
+- username
+- extension
+- password
+- from
+- to
+
+`to` is the phone number to receive SMS.
